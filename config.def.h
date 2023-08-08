64368e4542f53497c7911367fabd9084fd56ef02
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-08-08 18:36:12 +0700

    Remove comments

--- config.def.h
+++ config.def.h
@@ -1,29 +1,22 @@
-/* See LICENSE file for copyright and license details. */
-
-/* appearance */
 #include <X11/X.h>
-static const unsigned int borderpx = 1; /* border pixel of windows */
-static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int gappih = 2;   /* horiz inner gap between windows */
-static const unsigned int gappiv = 2;   /* vert inner gap between windows */
-static const unsigned int gappoh =
-    2; /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov =
-    2; /* vert outer gap between windows and screen edge */
-static int smartgaps =
-    1; /* 1 means no outer gap when there is only one window */
-static const unsigned int systraypinning =
-    0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
-          X */
-static const unsigned int systrayonleft =
-    0; /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2; /* systray spacing */
-static const int systraypinningfailfirst =
-    1; /* 1: if pinning fails, display systray on the first monitor, False:
-          display systray on the last monitor*/
-static const int showsystray = 1; /* 0 means no systray */
-static const int showbar = 1;     /* 0 means no bar */
-static const int topbar = 1;      /* 0 means bottom bar */
+
+static const unsigned int borderpx = 1;
+static const unsigned int snap = 32;
+
+static const unsigned int gappih = 2;
+static const unsigned int gappiv = 2;
+static const unsigned int gappoh = 2;
+static const unsigned int gappov = 2;
+static int smartgaps = 1;
+
+static const unsigned int systraypinning = 0;
+static const unsigned int systrayonleft = 0;
+static const unsigned int systrayspacing = 2;
+static const int systraypinningfailfirst = 1;
+static const int showsystray = 1;
+
+static const int showbar = 1;
+static const int topbar = 1;
 static const char *fonts[] = {"Ubuntu Nerd Font:pixelsize=14"};
 static const char dmenufont[] = "Ubuntu Nerd Font:pixelsize=14";
 static const char col_gray1[] = "#2e3440";
@@ -32,46 +25,32 @@ static const char col_gray3[] = "#d8dee9";
 static const char col_gray4[] = "#d8dee9";
 static const char col_gray5[] = "#3b4252";
 static const char *colors[][3] = {
-    /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
     [SchemeSel] = {col_gray4, col_gray5, col_gray5},
 };
 
-/* tagging */
 static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
-    /* xprop(1):
-     *	WM_CLASS(STRING) = instance, class
-     *	WM_NAME(STRING) = title
-     */
-    /* class      instance    title       tags mask     isfloating   monitor */
     {"Gimp", NULL, NULL, 0, 1, -1},
     {"Firefox", NULL, NULL, 1 << 8, 0, -1},
 };
 
-/* layout(s) */
-static const float mfact = 0.50; /* factor of master area size [0.05..0.95] */
-static const int nmaster = 1;    /* number of clients in master area */
-static const int resizehints =
-    1; /* 1 means respect size hints in tiled resizals */
-static const int attachdirection =
-    2; /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
-static const int lockfullscreen =
-    1; /* 1 will force focus on the fullscreen window */
-#define FORCE_VSPLIT                                                           \
-  1 /* nrowgrid layout: force two clients to always split vertically */
+static const float mfact = 0.50;
+static const int nmaster = 1;
+static const int resizehints = 1;
+static const int attachdirection = 2;
+static const int lockfullscreen = 1;
+#define FORCE_VSPLIT 1
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
-    /* symbol     arrange function */
-    {"[]=", tile}, /* first entry is default */
+    {"[]=", tile},
     {"[M]", monocle},
     {"HHH", grid},
-    {"><>", NULL}, /* no layout function means floating behavior */
+    {"><>", NULL},
 };
 
-/* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY, TAG)                                                      \
   {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
@@ -79,7 +58,6 @@ static const Layout layouts[] = {
       {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
       {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                                             \
   {                                                                            \
     .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
@@ -91,9 +69,7 @@ static const Layout layouts[] = {
 #define POWERMENU "$HOME/.dwm/scripts/powermenu"
 #define EDITOR "nvim"
 
-/* commands */
-static char dmenumon[2] =
-    "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] = "0";
 static const char *dmenucmd[] = {
     LAUNCHER, "-i",      "-m",  dmenumon,  "-fn", dmenufont, "-nb", col_gray1,
     "-nf",    col_gray3, "-sb", col_gray5, "-sf", col_gray4, NULL};
@@ -153,14 +129,9 @@ static const Key keys[] = {
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
             TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
     {MODKEY | ControlMask, XK_r, quit, {1}},
-
 };
 
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
- * ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-    /* click                event mask      button          function argument */
     {ClkTagBar, MODKEY, Button1, tag, {0}},
     {ClkTagBar, MODKEY, Button3, toggletag, {0}},
     {ClkWinTitle, 0, Button2, zoom, {0}},
