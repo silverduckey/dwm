9f88df4e1e06e2fd25cf735da78bab31c7497f8c
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-08-16 07:17:15 +0700

    Change a keymap

--- config.def.h
+++ config.def.h
@@ -88,7 +88,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = browsercmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_q,      spawn,          SHCMD("$HOME/.dwm/scripts/powermenu") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
