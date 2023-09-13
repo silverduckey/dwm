4342452a3357999324fdf2cc883c8e2292b48904
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-09-13 19:57:37 +0700

    Update autostart script

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+killall picom
+killall lxsession
 killall volumeicon
+killall ibus-daemon
+source $HOME/.screenlayout/screenlayout.sh
 slstatus &
-$HOME/.screenlayout/screenlayout.sh &
 feh --bg-fill --randomize ~/.wallpapers &
 picom &
 lxsession &
