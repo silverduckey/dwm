bb414ab469aff7b0063163e15c6dfd5cf94b3b26
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-09-15 21:03:43 +0700

    Use a smaller font

--- config.def.h
+++ config.def.h
@@ -15,7 +15,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Nerd Font:pixelsize=14" };
+static const char *fonts[]          = { "Ubuntu Nerd Font:pixelsize=12" };
 static const char col_gray1[]       = "#3b4252";
 static const char col_gray2[]       = "#3b4252";
 static const char col_gray3[]       = "#d8dee9";
