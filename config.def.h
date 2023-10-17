c64aa38ef41dbce54145e581e0fb18ad33a04b43
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-10-17 21:33:18 +0700

    Change some colors

--- config.def.h
+++ config.def.h
@@ -16,9 +16,9 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Ubuntu Nerd Font:size=10" };
-static const char col_gray1[]       = "#181825";
-static const char col_gray2[]       = "#181825";
-static const char col_gray3[]       = "#45475a";
+static const char col_gray1[]       = "#1e1e2e";
+static const char col_gray2[]       = "#1e1e2e";
+static const char col_gray3[]       = "#cdd6f4";
 static const char col_gray4[]       = "#45475a";
 static const char col_green[]       = "#a6e3a1";
 static const char *colors[][3]      = {
