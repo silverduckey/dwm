e8bf003498a1257c8fb544f505a87e984d0d3079
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2025-02-16 17:17:10 +0700

    Replace volumeicon with volctl

--- autostart.sh
+++ autostart.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 killall picom
 killall lxsession
-killall ibus-daemon
 killall nm-applet
-killall volumeicon
+killall volctl
+killall ibus-daemon
 killall slstatus
 . $HOME/.screenlayout/screenlayout.sh
 feh --bg-fill --randomize $HOME/.wallpapers &
 picom -b &
 lxsession &
-ibus-daemon -d &
 nm-applet &
-volumeicon &
+volctl &
+ibus-daemon -d &
 slstatus &
