94785c43656a3d901d71070b7018ca64dc72a978
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2025-02-16 17:01:32 +0700

    Revert "Change autostart file"
    This reverts commit 7f9b6c778875baf9ab6b8e4d2a55024463f79dbb.

--- autostart.sh
+++ autostart.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 killall picom
 killall lxsession
-killall volumeicon
 killall ibus-daemon
 killall nm-applet
+killall volumeicon
 killall slstatus
 . $HOME/.screenlayout/screenlayout.sh
 feh --bg-fill --randomize $HOME/.wallpapers &
 picom -b &
 lxsession &
-volumeicon &
 ibus-daemon -d &
 nm-applet &
+volumeicon &
 slstatus &
