a1745375794682df129192d0652c271aa8343359
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2025-02-16 14:57:20 +0700

    Change autostart file

--- autostart.sh
+++ autostart.sh
@@ -2,14 +2,14 @@
 killall picom
 killall lxsession
 killall ibus-daemon
-killall volumeicon
 killall nm-applet
+killall volumeicon
 killall slstatus
 . $HOME/.screenlayout/screenlayout.sh
 feh --bg-fill --randomize $HOME/.wallpapers &
 picom -b &
 lxsession &
 ibus-daemon -d &
-volumeicon &
 nm-applet &
+volumeicon &
 slstatus &
